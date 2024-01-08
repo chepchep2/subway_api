26bc061a08984828581ff290f07f8e3a280efcd6
Sangin Han <hugedreamhd@gmail.com>
2024-01-08 22:32:39 +0900

    feat: ui 수정중

--- lib/ui/widget/subway_item_widget.dart
+++ lib/ui/widget/subway_item_widget.dart
@@ -12,14 +12,21 @@ class SubwayItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.yellow[100],
+      decoration: BoxDecoration(
+        color: Colors.yellow[100],
         borderRadius: BorderRadius.circular(10),
         border: Border.all(color: Colors.black),
       ),
       child: ListTile(
         title: Text(subwayItem.stationName),
         subtitle: Text('종착역 : ${subwayItem.lastStation}'),
-        leading: Text(subwayItem.upDown),
+        leading: Text(
+          subwayItem.upDown,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+        ),
         trailing: Text(subwayItem.arrivalMsg),
       ),
     );
